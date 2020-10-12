ffd01e6678bcd47e96a99d41a9138596ea63932f
Ahmad Horyzat <171059@ppu.edu.ps>
2020-10-12 23:29:54 +0300

    migrate to database

--- ContactManager/Models/Contact.cs
+++ ContactManager/Models/Contact.cs
@@ -10,6 +10,21 @@ namespace ContactManager.Models
     [Table("Contacts")]
     public class Contact
     {
-
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set;}
+        [Required]
+        [StringLength(40)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(120)]
+        public string Address { get; set; }
+        [Required]
+        [StringLength(80)]
+        public string Email { get; set; }
+        [StringLength(20)]
+        public string Phone { get; set; }
     }
 }
